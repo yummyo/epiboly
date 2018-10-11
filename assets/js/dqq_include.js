042230cfcf894d7c0f252260ece5f50948e6b0c7
wangshuguang <smartwsg@163.com>
2018-10-11 13:34:36 +0800

    轮播图页面修改

--- assets/js/dqq_include.js
+++ assets/js/dqq_include.js
@@ -1,13 +1,8 @@
-<<<<<<< HEAD
 var _dir="/page/tpl/";
-document.write("<script src='/page/assets/js/jquery/jquery.min.js' type='text/javascript'></script>");
-document.write("<script src='/page/assets/js/validate/jquery.validate.min.js' type='text/javascript'></script>");
-=======
-var _dir="/tpl/";
 document.write("<script src='/page/assets/js/jquery/jquery.min.js' type='text/javascript'></script>");
 document.write("<script src='/page/assets/js/validate/jquery.validate.js' type='text/javascript'></script>");
 document.write("<script src='/page/assets/js/validate/additional-methods.js' type='text/javascript'></script>");
->>>>>>> 1b902a2af2369aaee90b1c1a0f96538982e7078e
+document.write("<script src='/page/assets/js/pagination.js' type='text/javascript'></script>");
 document.write("<script src='/page/assets/js/dqq_bong.js' type='text/javascript'></script>");
 document.write("<script src='/page/assets/js/layDate-v5.0.9/laydate.js' type='text/javascript'></script>");
 document.write("<script src='/page/assets/js/e.js' type='text/javascript'></script>");
